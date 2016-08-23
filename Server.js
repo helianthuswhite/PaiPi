0cc6ddd59782460d744344b9ca00dcd50bbbe56e
Littlewhite <964873559@qq.com>
2016-08-23 21:17:11 +0800

    完成首页

---
+++ Server.js
@@ -0,0 +1,46 @@
+/*
+	This is a server to load my front-end htmls.
+	So that I can open my project in any other devices.
+*/
+
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var mime = require('mime');
+
+//创建服务器
+http.createServer(function (req,res) {
+	// 解析请求
+	var pathname = url.parse(req.url).pathname;
+
+	var route = pathname.substr(1);
+
+	if (route == ''||route == null) {
+		route = 'index.html';
+	}
+	var URL = __dirname + '/'+ route;
+
+	fs.exists(URL,function(err){
+        if(!err){
+            res.writeHead(404,{'Content-Type':'text/html'});
+        }else{
+            fs.readFile(URL,function(err,data){
+                if(err){
+                    console.log('error: '+err);
+					res.writeHead(404,{'Content-Type':'text/html'});
+                }else{
+                    res.writeHead(200,{'content-type':mime.lookup(URL)});
+                    console.log(mime.lookup(URL));
+                    res.end(data.toString());
+                }
+            });//fs.readfile
+        }
+    })//path.exists
+	
+}).listen(2333);
+
+//控制台输出信息
+console.log('Server running at http://127.0.0.1:2333');
+
+
---
+++ js/my-app.js
@@ -0,0 +1,20 @@
+// Initialize your app
+var myApp = new Framework7();
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
+// 轮播图设置
+var mySwiper = myApp.swiper('.swiper-container', {
+  pagination:'.swiper-container .swiper-pagination',
+  spaceBetween: 50,
+  speed:400
+});
+
+
